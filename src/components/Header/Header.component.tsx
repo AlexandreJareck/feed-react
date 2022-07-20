ea849dc9f794fa1ffe885add0ef59c9624bee4b4
AlexandreJareck <alexandrejareck@gmail.com>
2022-07-19 21:38:58 -0300

    feat: created component header

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Strong, Container } from './Header.styles'
+
+export function Header() {
+  return (
+    <Container>
+      <Strong>Ignite Feed</Strong>
+    </Container>
+  )
+}
---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,4 @@
+import styled from 'styled-components'
+
+export const Strong = styled.strong``
+export const Container = styled.header``
